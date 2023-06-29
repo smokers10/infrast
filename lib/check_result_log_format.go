59503196ea9d9c20281efddb18b63d80f98d291f
nadzar mutaqin <nadzarmutaqin4@netmonk.id>
2023-06-30 01:44:47 +0700

    refactor log format

--- lib/check_result_log_format.go
+++ lib/check_result_log_format.go
@@ -10,13 +10,13 @@ func CheckResultLogFormat(results []contract.CheckResult) {
 	if len(results) == 0 {
 		fmt.Println("TSC result : OK!")
 	} else {
-		fmt.Println("TSC Result : your YAML configuration has problem see logs bellow :")
+		fmt.Printf("TSC Result : your YAML configuration has problem \n")
 		for _, v := range results {
 			fmt.Printf("table %s configuration :\n", v.TableName)
 			for _, v2 := range v.Mismatch {
 				fmt.Println(v2)
 			}
-			fmt.Printf("\n\n")
+			fmt.Printf("\n")
 		}
 	}
 }
