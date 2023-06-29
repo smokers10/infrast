fffc342ef7d4c53d62e8e47d7f9119f716c6e2a8
nadzar mutaqin <nadzarmutaqin4@netmonk.id>
2023-06-30 02:28:34 +0700

    refactor head to able call middleware & user management

--- head/head.go
+++ head/head.go
@@ -12,41 +12,53 @@ import (
 	"github.com/smokers10/go-infrastructure/jsonwebtoken"
 	"github.com/smokers10/go-infrastructure/lib"
 	"github.com/smokers10/go-infrastructure/mailer"
+	"github.com/smokers10/go-infrastructure/middleware"
 	tablestructurechecker "github.com/smokers10/go-infrastructure/table-structure-checker"
 	templateprocessor "github.com/smokers10/go-infrastructure/template-processor"
+	usermanagement "github.com/smokers10/go-infrastructure/user-management"
+	usermanagementrepository "github.com/smokers10/go-infrastructure/user-management-repository"
 )
 
-type ModuleHeader struct {
-	DB                contract.DatabaseContract
-	Encryption        contract.EncryptionContract
-	Identfier         contract.IdentfierContract
-	JWT               contract.JsonWebTokenContract
-	Mailer            contract.MailerContract
-	TemplateProcessor contract.TemplateProcessor
-	Configuration     *config.Configuration
+type Head struct {
+	Module module
 }
 
-func Head(path string) (*ModuleHeader, error) {
+type module struct {
+	DB                       contract.DatabaseContract
+	Encryption               contract.EncryptionContract
+	Identfier                contract.IdentfierContract
+	JWT                      contract.JsonWebTokenContract
+	Mailer                   contract.MailerContract
+	TemplateProcessor        contract.TemplateProcessor
+	UserManagementRepository contract.UserManagementRepository
+	Configuration            *config.Configuration
+}
+
+func (h *Head) Initialize(path string) (*module, error) {
 	ch := config.ConfigurationHead()
 	config, err := ch.Read(path)
 	if err != nil {
 		return nil, err
 	}
 
-	result := ModuleHeader{
-		DB:                database.Database(config),
-		Encryption:        encryption.Encryption(),
-		Identfier:         identifier.Identifier(config),
-		JWT:               jsonwebtoken.JsonWebToken(config),
-		Mailer:            mailer.Mailer(config),
-		TemplateProcessor: templateprocessor.TemplateProccessor(),
-		Configuration:     config,
-	}
-
-	sql, err := result.DB.PosgresSQL()
+	database := database.Database(config)
+	sql, err := database.PosgresSQL()
 	if err != nil {
 		return nil, fmt.Errorf("error call postgre db : %v", err.Error())
 	}
+
+	modules := module{
+		DB:                       database,
+		Encryption:               encryption.Encryption(),
+		Identfier:                identifier.Identifier(config),
+		JWT:                      jsonwebtoken.JsonWebToken(config),
+		Mailer:                   mailer.Mailer(config),
+		TemplateProcessor:        templateprocessor.TemplateProccessor(),
+		UserManagementRepository: usermanagementrepository.UserManagementRepository(sql),
+		Configuration:            config,
+	}
+
+	h.Module = modules
 
 	checkerRepo := tablestructurechecker.TableStructureCheckerRepository(sql)
 	checker := tablestructurechecker.TableStructureChecker(checkerRepo)
@@ -60,5 +72,23 @@ func Head(path string) (*ModuleHeader, error) {
 		return nil, errors.New("user management TSC error")
 	}
 
-	return &result, nil
+	return &modules, nil
+}
+
+func (h *Head) Middleware(userType string) (contract.Middleware, error) {
+	m, err := middleware.Middleware(&h.Module.Configuration.UserManagement, h.Module.UserManagementRepository, h.Module.JWT, userType)
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func (h *Head) UserManagement(userType string) (contract.UserManagement, error) {
+	UM, err := usermanagement.UserManagement(h.Module.Configuration, h.Module.UserManagementRepository, h.Module.Identfier, h.Module.Encryption, h.Module.JWT, h.Module.Mailer, h.Module.TemplateProcessor, userType)
+	if err != nil {
+		return nil, err
+	}
+
+	return UM, nil
 }
